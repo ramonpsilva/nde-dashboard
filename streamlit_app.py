b1cb8be23cc2b02acc9305e413889a184fe9b72d
ramonpsilva <29004452+ramonpsilva@users.noreply.github.com>
2025-06-11 20:19:42 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,67 @@
-import streamlit as st
 import pandas as pd
-import math
-from pathlib import Path
+import plotly.express as px
+import streamlit as st
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='Engenharia Civil dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
+# Carrega a oferta das disciplinas oferecidas pelo DEES entre 2012/2 a 2024/1.
+oferta = pd.read_excel('ofertaConsolidadaTratada.xlsx')
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
+semestres = oferta['semestre'].unique()
+departamentos = oferta.sort_values(by='ofertante')['ofertante'].unique()
+departamentos.sort()
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+oferta['disciplina'] = oferta.apply(lambda row: row.codigo + ": " + row.nome, axis=1)
+disciplinas = pd.DataFrame(oferta.drop(columns=['curso', 'professor', 'ch_prof', 'horario', 'insc']))
+disciplinas.reset_index(inplace=True, drop=True)
+disciplinas.sort_values(by='disciplina', inplace=True)
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+doi = ['EES', 'EHR', 'EMC', 'ESA', 'ETG']
+half = ['ETG012', 'ETG016', 'EMC015', 'EMC016']
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+# calculo geral de disciplinas para gráficos de ocupação
+df = pd.DataFrame(columns=['semestre', 'disciplina', 'ofertante', 'vagas', 'ocupacao', 'nt'])
+for sem in semestres:
+    disciplinas_sem = oferta[oferta['semestre'] == sem]
+    for cur in departamentos:
+        disciplina_depto = disciplinas_sem[disciplinas_sem['ofertante'] == cur]
+        disciplina_codigos = disciplina_depto['codigo'].unique()
+        for cod in disciplina_codigos:
+            somaocupa = disciplina_depto.loc[disciplina_depto['codigo'] == cod, 'ocupacao'].sum()
+            somavagas = disciplina_depto.loc[disciplina_depto['codigo'] == cod, 'vagas'].sum()
+            somaturmas = disciplina_depto.loc[disciplina_depto['codigo'] == cod, 'turma'].count()
+            d = disciplina_depto.loc[disciplina_depto['codigo'] == cod]['disciplina'].unique().tolist()[0]
+            df.loc[len(df.index)] = [sem, d, cur, somavagas, somaocupa, somaturmas]
+df.sort_values(by='semestre', inplace=True)
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+st.title("Análise de Ocupação de Disciplinas")
 
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
+tab1, tab2 = st.tabs(["Ocupação Geral", "Departamentos Civil"])
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
+with tab1:
+    st.header("Departamentos")
+    depto = st.selectbox("Selecione o departamento:", departamentos)
+    disc_opts = disciplinas[disciplinas['ofertante'] == depto]['disciplina'].unique()
+    disc = st.selectbox("Selecione a disciplina:", disc_opts)
+    dfc = df[df['ofertante'] == depto]
+    df1 = dfc[dfc['disciplina'] == disc]
+    fig = px.line(df1, x='semestre', y=['vagas', 'ocupacao'], title=disc)
+    fig.update_layout(legend_title_text='Ocupação')
+    fig.update_traces({'name': 'vagas'}, selector={'name': 'vagas'})
+    fig.update_traces({'name': 'ocupação'}, selector={'name': 'ocupacao'})
+    fig.update_xaxes(zeroline=True)
+    fig.update_yaxes(zeroline=True)
+    st.plotly_chart(fig, use_container_width=True)
 
-    return gdp_df
-
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: Engenharia Civil dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
-
-
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+with tab2:
+    st.header("Departamentos Civil")
+    depto_civil = st.radio("Selecione o departamento civil:", doi, horizontal=True)
+    disc_opts2 = disciplinas[disciplinas['ofertante'] == depto_civil]['disciplina'].unique()
+    disc2 = st.selectbox("Selecione a disciplina:", disc_opts2, key="disc2")
+    dfc2 = df[df['ofertante'] == depto_civil]
+    df2 = dfc2[dfc2['disciplina'] == disc2]
+    fig2 = px.line(df2, x='semestre', y=['vagas', 'ocupacao'], title=disc2)
+    fig2.update_layout(legend_title_text='Ocupação')
+    fig2.update_traces({'name': 'vagas'}, selector={'name': 'vagas'})
+    fig2.update_traces({'name': 'ocupação'}, selector={'name': 'ocupacao'})
+    fig2.update_xaxes(zeroline=True)
+    fig2.update_yaxes(zeroline=True, range=[0, 300])
+    st.plotly_chart(fig2, use_container_width=True)
